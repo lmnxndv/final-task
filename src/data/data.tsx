893e0f1c37d0c6d16e33126032b8a91977a92102
lmnxndv <lilyakhundi@gmail.com>
2023-11-07 15:11:44 +0400

    izmenenie v select

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,23 +1,55 @@
-// import { string } from "yup";
+interface IOptionsData {
+  sections: OptionsSection[];
+}
+interface OptionsSection {
+  id: number;
+  title: string;
+  options: OptionsLabel[];
+}
+interface OptionsLabel {
+  label: string;
+  value: string;
+}
 
-// export interface IRegisterData {
-//   sections: IRegisterSection[];
-// }
+const optionsData: IOptionsData = {
+  sections: [
+    {
+      id: 1,
+      title: "Təyinat",
+      options: [
+        { label: "Ümümi", value: "Ümümi" },
+        { label: "Apellasiya şurası", value: "Apellasiya şurası" },
+        { label: "Təhlükəsizlik şurası", value: "Təhlükəsizlik şurası" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Təsnifat",
+      options: [
+        { label: "Digər", value: "Digər" },
+        { label: "Göstəriş məktubu", value: "Göstəriş məktubu" },
+        { label: "Metodiki tövsiyyə", value: "Metodiki tövsiyyə" },
+      ],
+    },
+    {
+      id: 3,
+      title: "Nomenklatur",
+      options: [
+        { label: "12.1", value: "12.1" },
+        { label: "12.2", value: "12.2" },
+        { label: "12.3", value: "12.3" },
+      ],
+    },
+    {
+      id: 4,
+      title: "Mezmun",
+      options: [
+        { label: "Test-1", value: "test-1" },
+        { label: "Test-2", value: "test-2" },
+        { label: "Test-3", value: "test-3" },
+      ],
+    },
+  ],
+};
 
-// export interface IRegisterSection {
-//   title?: RegisterValue[];
-// }
-// export interface RegisterValue {
-//   value: string;
-//   labale:string
-// }
-
-// // export const IRegisterData: IRegisterData = {
-// //   sections:[
-// //     {
-// //         title: [
-// //             {value:string},
-// //         ]
-// //     }
-// //   ]
-// // };
+export default optionsData;
--- src/pages/firstPage/index.tsx
+++ src/pages/firstPage/index.tsx
@@ -2,6 +2,7 @@ import { Form, Select, Checkbox } from "antd";
 import { CaretDownOutlined } from "@ant-design/icons";
 import type { CheckboxChangeEvent } from "antd/es/checkbox";
 import "./style.css";
+import optionsData from "../../data/data";
 
 const handleChange = (value: string) => {
   console.log(`selected ${value}`);
@@ -16,87 +17,20 @@ const FirstPage = () => {
       <div className="firstPage-row">
         <h2>Əsas məlumatlar</h2>
         <Form>
-          <Form.Item label="Təyinat">
-            <Select
-              id="Təyinat"
-              style={{ width: 300 }}
-              suffixIcon={<CaretDownOutlined />}
-              onChange={handleChange}
-              options={[
-                {
-                  options: [
-                    { label: "Ümümi", value: "Ümümi" },
-                    { label: "Apellasiya şurası", value: "Apellasiya şurası" },
-                    {
-                      label: "Təhlükəsizlik şurası",
-                      value: "Təhlükəsizlik şurası",
-                    },
-                  ],
-                },
-              ]}
-            ></Select>
-          </Form.Item>
-          <Form.Item label="Təsnifat">
-            <Select
-              id="Təsnifat"
-              style={{ width: 300 }}
-              suffixIcon={<CaretDownOutlined />}
-              onChange={handleChange}
-              options={[
-                {
-                  options: [
-                    { label: "Digər", value: "Digər" },
-                    { label: "Göstəriş məktubu", value: "Göstəriş məktubu" },
-                    {
-                      label: "Metodiki tövsiyyə",
-                      value: "Metodiki tövsiyyə",
-                    },
-                  ],
-                },
-              ]}
-            ></Select>
-          </Form.Item>
-          <Form.Item label="Nomenklatur">
-            <Select
-              id="Təsnifat"
-              style={{ width: 300 }}
-              suffixIcon={<CaretDownOutlined />}
-              disabled
-              options={[
-                {
-                  options: [
-                    { label: "12.1", value: "Digər" },
-                    { label: "12.2", value: "Göstəriş məktubu" },
-                    {
-                      label: "12.3",
-                      value: "Metodiki tövsiyyə",
-                    },
-                  ],
-                },
-              ]}
-            ></Select>
-            <Checkbox onChange={onChange}>Konfidensial</Checkbox>
-          </Form.Item>
-          <Form.Item label="Mezmun">
-            <Select
-              id="Mezmun"
-              style={{ width: 300 }}
-              suffixIcon={<CaretDownOutlined />}
-              onChange={handleChange}
-              options={[
-                {
-                  options: [
-                    { label: "Test-1", value: "test-1" },
-                    { label: "Test-2", value: "test-2" },
-                    {
-                      label: "Test-3",
-                      value: "test-3",
-                    },
-                  ],
-                },
-              ]}
-            ></Select>
-          </Form.Item>
+          {optionsData.sections.map((item) => (
+            <Form.Item key={item.id} label={item.title}>
+              <Select
+                id={item.title}
+                style={{ width: 300 }}
+                suffixIcon={<CaretDownOutlined />}
+                onChange={handleChange}
+                options={item.options.map((option) => ({
+                  label: option.label,
+                  value: option.value,
+                }))}
+              ></Select>
+            </Form.Item>
+          ))}
         </Form>
       </div>
     </div>
